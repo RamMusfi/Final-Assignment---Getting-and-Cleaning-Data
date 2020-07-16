696b9cc998a861af45d97c44cf3f36c4f1f433dc
RamMusfi <63108823+RamMusfi@users.noreply.github.com>
2020-07-16 21:25:09 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,12 @@
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
 ## Merging training with test sets into one data set
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
